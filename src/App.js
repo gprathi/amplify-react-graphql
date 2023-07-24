46a7cf5034effa4ffd9904db121306700baa2f91
Govind Prathi <govind.prathi2@wnco.com>
2023-07-24 00:49:05 -0500

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import { LeadCreateForm } from './ui-components';
-function App() {
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
+    <div  className="App">
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
       <LeadCreateForm />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,30 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://d2xc2tbeuzcfxbia2bwzgjklda.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-3tdpamzi6zg6xf7go6i3nhbxym"
+    "aws_appsync_apiKey": "da2-3tdpamzi6zg6xf7go6i3nhbxym",
+    "aws_cognito_identity_pool_id": "us-east-1:927460ca-b271-4a67-afa1-8a1a4852cbf9",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_xjKYcEqz0",
+    "aws_user_pools_web_client_id": "1funs7ser7t7930a047d386g2o",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
 };
 
 
