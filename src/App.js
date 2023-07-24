af3d8eababf3c4564e14dc7ed688167ed65b2757
Govind Prathi <govind.prathi2@wnco.com>
2023-07-23 19:23:59 -0500

    deploy Creatform

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import { LeadCreateForm } from './ui-components';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to Amplify. This is a test 2.
-        </p>
-      </header>
       <LeadCreateForm />
     </div>
   );
